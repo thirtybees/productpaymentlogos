c2983008ccf08f118e1548a239a61916ba153e35
Markus Hitter <mah@jump-ing.de>
2018-05-15 15:37:22 +0200

    Standardize module name.
    This is, all uppercase and 'Block' to the front.

--- productpaymentlogos.php
+++ productpaymentlogos.php
@@ -40,7 +40,7 @@ class ProductPaymentLogos extends Module
 		$this->bootstrap = true;
 		parent::__construct();
 
-		$this->displayName = $this->l('Product payment logos block');
+		$this->displayName = $this->l('Block Product Payment Logos');
 		$this->description = $this->l('Displays the logos of the available payment systems on the product page.');
 
 		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => '1.6.99.99');
