bdc773c21dbb31202ee82da9cf5796a0c220ab9d
Petr Hučík <petr@getdatakick.com>
2024-05-02 08:46:56 +0200

    Release productpaymentlogos v2.1.0

--- productpaymentlogos.php
+++ productpaymentlogos.php
@@ -37,7 +37,7 @@ class ProductPaymentLogos extends Module
     {
         $this->name = 'productpaymentlogos';
         $this->tab = 'front_office_features';
-        $this->version = '2.0.2';
+        $this->version = '2.1.0';
         $this->author = 'thirty bees';
         $this->need_instance = 0;
         $this->bootstrap = true;
